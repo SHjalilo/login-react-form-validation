d6058cd858a8e801bb4f60c0c3487c265e7c1976
SHjalilo <aaaabdjalil@gmail.com>
2023-08-08 03:31:19 +0200

    signup form design form is done !

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,34 @@
-
+import { Link } from "react-router-dom";
 
 
 const Login = ()=>{
 
     return(
-        <>
-        <p>Login Form</p>
-        </>
+        <div className='login template d-flex justify-content-center align-items-center 100-w vh-100 bg-primary'>
+        <div className="50-w p-5 rounded bg-white">
+            <form>
+            <h3 className="text-center">Sign In</h3>
+            <div className="mb-2">
+                <label htmlFor="email">Email : </label>
+                <input type="email" placeholder='Enter email' className='form-control' />
+            </div>
+            <div className="mb-2">
+                <label htmlFor="password">Password : </label>
+                <input type="password" placeholder='Enter password' className='form-control' />
+            </div>
+            <div className="mb-2">
+                <input type="checkbox" id='check' className='costom-control costom-checkbox' />
+                <label htmlFor="check" className='costom-input-label ms-2'> Remember me </label>
+            </div>
+            <div className="d-grid">
+                <button className='btn btn-primary'>Sing In</button>
+            </div>
+            <p className="text-end mt-2">
+                Forgot <Link to="/" className="ms-2">Password?</Link> <Link to="/signup" className="ms-2">Sign Up</Link>
+            </p>
+            </form>
+        </div>
+    </div>
     )
 }
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,12 +1,39 @@
+import { Link } from "react-router-dom";
 
 
 
 const SignUp = ()=>{
 
     return(
-        <>
-          <p>Sign-up Form</p>
-        </>
+        <div className='signup template d-flex justify-content-center align-items-center 100-w vh-100 bg-primary'>
+            <div className="50-w p-5 rounded bg-white">
+                <form>
+                <h3 className="text-center">Sign Up</h3>
+                <div className="mb-2">
+                    <label htmlFor="fname">First Name : </label>
+                    <input type="text" placeholder='Enter First Name' className='form-control' />
+                </div>
+                <div className="mb-2">
+                    <label htmlFor="lname">Last Name : </label>
+                    <input type="text" placeholder='Enter Last Name' className='form-control' />
+                </div>
+                <div className="mb-2">
+                    <label htmlFor="email">Email : </label>
+                    <input type="email" placeholder='Enter email' className='form-control' />
+                </div>
+                <div className="mb-2">
+                    <label htmlFor="password">Password : </label>
+                    <input type="password" placeholder='Enter password' className='form-control' />
+                </div>
+                <div className="d-grid mt-2">
+                    <button className='btn btn-primary'>Sing Up</button>
+                </div>
+                <p className="text-end mt-2">
+                   Already Registred <Link to="/" className="ms-2">Sign In</Link>
+                </p>
+                </form>
+            </div>
+        </div>
     )
 }
 
